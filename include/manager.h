ba50d69745aa74a5839352ec9bbf10f4ffa4bace
hydroph <qiu.ke@qq.com>
2019-09-29 15:07:08 +0800

    resource manager structure

--- include/manager.h
+++ include/manager.h
@@ -7,12 +7,22 @@
 #include "entities.h"
 #include "light.h"
 #include "color.h"
+#include "camera.h"
+#include "presenter.h"
 
 class ResourceManager {
 public:
-
+    void render();
 private:
-
+    std::vector<Material*> vecMaterial;
+    std::vector<Entity*>   vecEntity;
+    std::vector<Light*>    vecLight;
+    Material** pMaterials = nullptr;
+    Entity**   pEntities = nullptr;
+    Light**    pLights = nullptr;
+    Camera*    pCamera = nullptr;
+    Presenter* pPresenter = nullptr;
+    int countMaterial = 0, countEntity = 0, countLight = 0;
 };
 
 #endif
