440ed93e923b164b81159c21dec60f4f8d1eb4b5
QiuKe <qiuk17@mails.tsinghua.edu.cn>
2019-09-29 09:20:30 +0800

    add windows support

--- src/materials.cpp
+++ src/materials.cpp
@@ -1,5 +1,9 @@
 #include "materials.h"
 
+#ifdef _WIN32
+#include <corecrt_math_defines.h>
+#endif
+
 Color LambertMaterial::getColor(const Collision& collision, const Light& light) const {
     auto interaction = light.castOnPoint(collision.point);
     return ((color * interaction.color) * Vector3f::dot(collision.normal, interaction.direction) * M_1_PI * (1 - absorption)).restriction();
