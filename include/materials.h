bf6d04c4e805e774b009369f4725fdb117ddb934
qiuk17 <qiuk17@mails.tsinghua.edu.cn>
2019-11-02 23:23:39 +0800

    add openGL support for materials

--- include/materials.h
+++ include/materials.h
@@ -9,12 +9,14 @@
 class Material {
 public:
     virtual Color getColor(const Collision& collision, const Light& light) const = 0;
+    virtual void glApply() const = 0;
 };
 
 class DiffMaterial : public Material {
 public:
     DiffMaterial(const Color& color_, float absorption_ = 0.0f) : color(color_), absorption(absorption_) {}
     Color getColor(const Collision& collision, const Light& light) const override;
+    void glApply() const override;
 private:
     Color color;
     float absorption;
@@ -23,9 +25,12 @@ private:
 class PhongMaterial : public Material {
 public:
     PhongMaterial(const Color& color_, float shininess_) : color(color_), shininess(shininess_) {}
+    PhongMaterial(const Color& color_, const Color& specColor_, float shininess_) : color(color_), specColor(specColor_), shininess(shininess_) {}
     Color getColor(const Collision& collision, const Light& light) const override;
+    void glApply() const override;
 private:
     Color color;
+    Color specColor;
     float shininess;
 };
 
--- src/materials.cpp
+++ src/materials.cpp
@@ -1,5 +1,7 @@
 #include "materials.h"
 #include <algorithm>
+#include <GL/glut.h>
+
 #ifdef _WIN32
 #include <corecrt_math_defines.h>
 #endif
@@ -13,4 +15,14 @@ Color PhongMaterial::getColor(const Collision& collision, const Light& light) co
     auto interaction = light.castOnPoint(collision.point);
     return ((color * interaction.color * Vector3f::dot(interaction.direction, collision.normal)).restriction()
             + (interaction.color * std::min(1.0f, shininess / 10) * powf(std::max(Vector3f::dot((2 * Vector3f::dot(collision.normal, interaction.direction) * collision.normal - interaction.direction), -collision.pRay->direction), 0.0f), shininess)).restriction());
+}
+
+void DiffMaterial::glApply() const {
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, Vector4f(color.toVec(), 1.0f));
+}
+
+void PhongMaterial::glApply() const {
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, Vector4f(color.toVec(), 1.0f));
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, Vector4f(specColor.toVec(), 1.0f));
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, Vector2f(shininess * 4.0, 1.0f));
 }
