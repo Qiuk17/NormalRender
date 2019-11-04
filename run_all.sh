5220b8235fe3099ab4a0be1d9b89a74b0adfe4e8
hydroph <qiu.ke@qq.com>
2019-11-04 10:05:40 +0800

    new run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# If project not ready, generate cmake file.
+if [[ ! -d build ]]; then
+    mkdir -p build
+    cd build
+    cmake ..
+    cd ..
+fi
+
+# Build project.
+cd build
+make -j
+cd ..
+
+# Run all testcases. 
+# You can comment some lines to disable the run of specific examples.
+mkdir -p output
+for i in $(ls testcases)
+do
+	bin/PA2 $(echo testcases/$i output/$i.bmp)
+done
+
